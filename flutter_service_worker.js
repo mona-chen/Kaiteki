5f1ef069d04c4243fa4e0744c81826f0d9bb89b3
Craftplacer <Craftplacer@users.noreply.github.com>
2021-12-18 19:56:39 +0000

    deploy: 29f1729f3dad6cde7503e29fe6712c0434480a1f

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -7,7 +7,7 @@ const RESOURCES = {
 "canvaskit/profiling/canvaskit.js": "3783918f48ef691e230156c251169480",
 "canvaskit/profiling/canvaskit.wasm": "6d1b0fc1ec88c3110db88caa3393c580",
 "canvaskit/canvaskit.wasm": "b179ba02b7a9f61ebc108f82c5a1ecdb",
-"main.dart.js": "cc755719cfac4fc61e78ee4b1ffe7957",
+"main.dart.js": "deedfa776aa2ce0f67c262b397136d8a",
 "version.json": "6e025104e1a93a15ca8c71df0b6489fe",
 "manifest.json": "3d207c80129d9c56e05666c72b4a3ae2",
 "icons/monochrome_icon_x16.png": "8f1ca974aadfea0b34b94e113cb04597",
@@ -55,8 +55,8 @@ const RESOURCES = {
 "assets/packages/wakelock_web/assets/no_sleep.js": "7748a45cd593f33280669b29c2c8919a",
 "assets/packages/mdi/fonts/materialdesignicons-webfont.ttf": "3e722fd57a6db80ee119f0e2c230ccff",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"index.html": "c95bc3c963136fe2bc64f7a5adf2cf0e",
-"/": "c95bc3c963136fe2bc64f7a5adf2cf0e",
+"index.html": "2f0bfa6a57129f7c56d5352fc4d880e1",
+"/": "2f0bfa6a57129f7c56d5352fc4d880e1",
 "favicon.png": "4d0f0dd24e16e8faf0f281ac7a6184c7"
 };
 
