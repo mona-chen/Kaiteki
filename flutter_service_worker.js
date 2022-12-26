4048b0e1ac56b930e5c399205f9a17b56bdba9c0
Craftplacer <Craftplacer@users.noreply.github.com>
2022-12-26 05:26:59 +0000

    deploy: 59a8c58ab929ff6f2473bbdf487c6ee9ca5c4191

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -69,8 +69,8 @@ const RESOURCES = {
 "assets/assets/fonts/Quicksand-Medium.ttf": "865b25e449878b6b4d1f9e098ff2f510",
 "assets/assets/oauth-success.html": "2eae47cadd2aaa96b32d6a46844b969d",
 "assets/NOTICES": "c92a098db6bc5fbb5f3c19f7632ed91b",
-"index.html": "2303ccc94b394084b6fb2db9e4b10f76",
-"/": "2303ccc94b394084b6fb2db9e4b10f76",
+"index.html": "c9d80fd3f8a2c083d60fb34d724c312c",
+"/": "c9d80fd3f8a2c083d60fb34d724c312c",
 "favicon.png": "4d0f0dd24e16e8faf0f281ac7a6184c7",
 "version.json": "a9c38837cf7157343a06ce5c0e767510"
 };
